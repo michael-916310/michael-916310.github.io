ee17ea4fa7da57e71e647fbc81456d3babec0f17
michael-916310 <916310@rambler.ru>
2020-04-26 10:47:00 +0400

    add tutorial

---
+++ Michael/react/tutorial/precache-manifest.670438ef21eb38b35a99d3ecb66e6efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "949b3b338aa473ac26cceef772ba24ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da26b580d878c081f9cc",
+    "url": "/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "97a4cc1b0a442dea4095",
+    "url": "/static/js/2.05a69b39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05a69b39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da26b580d878c081f9cc",
+    "url": "/static/js/main.320b2caa.chunk.js"
+  },
+  {
+    "revision": "3223abbd197b5e36bc69",
+    "url": "/static/js/runtime-main.491a5962.js"
+  }
+]);
