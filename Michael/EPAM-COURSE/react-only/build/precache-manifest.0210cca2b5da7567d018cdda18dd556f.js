47fd083cb511e90d4afe5b021a160d14b04dbb86
michael-916310 <916310@rambler.ru>
2020-06-02 16:59:35 +0400

    add homepage

---
+++ Michael/EPAM-COURSE/react-only/build/precache-manifest.0210cca2b5da7567d018cdda18dd556f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd4337eb533156c87e189b6f2f344bd",
+    "url": "/Michael/EPAM-COURSE/react-only/build/index.html"
+  },
+  {
+    "revision": "12e0a6e6f3fade0aad53",
+    "url": "/Michael/EPAM-COURSE/react-only/build/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5a4d0034e7624da97ba6",
+    "url": "/Michael/EPAM-COURSE/react-only/build/static/css/main.d84c1f3b.chunk.css"
+  },
+  {
+    "revision": "12e0a6e6f3fade0aad53",
+    "url": "/Michael/EPAM-COURSE/react-only/build/static/js/2.8d639ac4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Michael/EPAM-COURSE/react-only/build/static/js/2.8d639ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4d0034e7624da97ba6",
+    "url": "/Michael/EPAM-COURSE/react-only/build/static/js/main.49b38533.chunk.js"
+  },
+  {
+    "revision": "2f259d131f31412b81fc",
+    "url": "/Michael/EPAM-COURSE/react-only/build/static/js/runtime-main.28f63539.js"
+  }
+]);
--- Michael/EPAM-COURSE/react-only/build/service-worker.js
+++ Michael/EPAM-COURSE/react-only/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81cfa1a7d73e56eacd8a937572ae6398.js"
+  "/Michael/EPAM-COURSE/react-only/build/precache-manifest.0210cca2b5da7567d018cdda18dd556f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Michael/EPAM-COURSE/react-only/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
