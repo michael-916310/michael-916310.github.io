df5a0558cf7b760976a7ba9b6350b9e4cf46b215
michael-916310 <916310@rambler.ru>
2020-06-02 19:59:05 +0400

    remane-2

---
+++ michael/react/events1/precache-manifest.8cb28f67dd6af0f74abc452601caa283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07e631f36b0d6c5cbb185f251b70d7a",
+    "url": "/Michael/react/events1/index.html"
+  },
+  {
+    "revision": "35cbc430dc7ff97ab5c8",
+    "url": "/Michael/react/events1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "15781d7bdcc0e4a00c65",
+    "url": "/Michael/react/events1/static/js/2.c07b32ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Michael/react/events1/static/js/2.c07b32ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35cbc430dc7ff97ab5c8",
+    "url": "/Michael/react/events1/static/js/main.d57d81d7.chunk.js"
+  },
+  {
+    "revision": "47719dc2f7a07288c9dd",
+    "url": "/Michael/react/events1/static/js/runtime-main.c9cc26ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Michael/react/events1/static/media/logo.5d5d9eef.svg"
+  }
+]);
